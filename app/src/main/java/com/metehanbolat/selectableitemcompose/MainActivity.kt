fbdcca7724e3d097954bf7c501e26bf2ada51ba6
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-22 00:25:44 +0300

    SelectableItem created.

--- app/src/main/java/com/metehanbolat/selectableitemcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/selectableitemcompose/MainActivity.kt
@@ -3,6 +3,17 @@ package com.metehanbolat.selectableitemcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.selectableitemcompose.ui.theme.SelectableItemComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +21,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SelectableItemComposeTheme {
-
+                var selected by remember { mutableStateOf(false) }
+                var selected2 by remember { mutableStateOf(false) }
+                
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(80.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    SelectableItem(
+                        selected = selected,
+                        title = "Lorem Ipsum",
+                        onClick = {
+                            selected = !selected
+                        }
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/metehanbolat/selectableitemcompose/SelectableItem.kt
@@ -0,0 +1,92 @@
+package com.metehanbolat.selectableitemcompose
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SelectableItem(
+    modifier: Modifier = Modifier,
+    selected: Boolean,
+    title: String,
+    titleColor: Color = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface.copy(alpha = 0.2f),
+    titleSize: TextUnit = MaterialTheme.typography.h6.fontSize,
+    titleWeight: FontWeight = FontWeight.Medium,
+    subtitle: String? = null,
+    subtitleColor: Color = if (selected) MaterialTheme.colors.onSurface else MaterialTheme.colors.onSurface.copy(alpha = 0.2f),
+    borderWidth: Dp = 1.dp,
+    borderColor: Color = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface.copy(alpha = 0.2f),
+    borderShape: Shape = RoundedCornerShape(size = 10.dp),
+    icon: ImageVector = Icons.Default.CheckCircle,
+    iconColor: Color = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface.copy(0.2f),
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .border(
+                width = borderWidth,
+                color = borderColor,
+                shape = borderShape
+            )
+            .clickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.weight(8f),
+                text = title,
+                style = TextStyle(
+                    color = titleColor,
+                    fontSize = titleSize,
+                    fontWeight = titleWeight
+                ),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            IconButton(
+                modifier = Modifier.weight(2f),
+                onClick = onClick
+            ) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = "Selectable Item Icon",
+                    tint = iconColor
+                )
+            }
+        }
+        if (subtitle != null) {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .padding(bottom = 12.dp),
+                text = subtitle,
+                style = TextStyle(color = subtitleColor),
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
